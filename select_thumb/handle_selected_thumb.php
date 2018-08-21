93b6b88f9a9f79916248cc7b6afead27cb2c9770
tzubeli <avitaltzubeli@gmail.com>
2018-08-21 15:47:29 +0300

    require

--- select_thumb/handle_selected_thumb.php
+++ select_thumb/handle_selected_thumb.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__).'/../kaltura_config.php');
+require_once(dirname(__FILE__).'/../config.php');
 
 $embeddedUrl = $_POST['url'];
 $thumbnailUrl = $_POST['thumbnailUrl'];
